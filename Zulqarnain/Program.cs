42a3d003ee6ea7245871524810a3a9fe6b683015
Dell <Dell@MSI-GS65>
2024-09-14 10:12:13 -0700

    Add project files.

---
+++ Zulqarnain/Program.cs
@@ -0,0 +1,16 @@
+﻿class zulqarnain
+{
+    static void Main(string[] args)
+    {
+
+        Console.WriteLine("Enter your name");
+        string name = Console.ReadLine();
+        Console.WriteLine("Enter your roll number");
+        string roll = Console.ReadLine();
+        Console.WriteLine("Enter your Class and Section");
+        string section = Console.ReadLine();
+        Console.WriteLine("Your name is : " + name);
+        Console.WriteLine("Your roll no is : " + roll);
+        Console.WriteLine("Your Class and Swction is : " + section);
+    }
+}
